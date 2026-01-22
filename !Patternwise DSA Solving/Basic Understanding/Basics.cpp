f747be35ae34e8b44d09ac9731925bb449cb9c56
Shubham Mahajan <mhajan.shubh@gmail.com>
2026-01-23 00:05:43 +0530

    Starting

---
+++ !Patternwise DSA Solving/Basic Understanding/Basics.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int shubhamMahajan(int n)
+{
+    int doublework = 2 * n;
+    cout << doublework << endl;
+}
+
+int main()
+{
+    int m = 10;
+    shubhamMahajan(m);
+}
